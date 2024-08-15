3dfe4b0b2094993a742d45f991b5e57b93a31675
Dapo Adedire <adedireadedapo19@gmail.com>
2024-08-15 11:22:29 +0100

    refactor: Update welcome and help messages with more descriptive and engaging content

--- main.go
+++ main.go
@@ -15,21 +15,25 @@ func sendMediaGroup(bot *tgbotapi.BotAPI, chatID int64, imageURL string) {
     photo := tgbotapi.NewInputMediaPhoto(tgbotapi.FilePath(imageURL))
     mediaGroup := tgbotapi.NewMediaGroup(chatID, []interface{}{photo})
 
-    // Send the photo
     messages, err := bot.SendMediaGroup(mediaGroup)
     if err != nil {
-        log.Panic(err)
+        log.Printf("Error sending media group: %v", err)
+        return
     }
 
-    // Handle the response messages if needed
     for _, msg := range messages {
         log.Printf("Sent message: %v", msg)
     }
 }
 
+func getRandomImage() string {
+    randomNumber := rand.Intn(1000) + 1
+    return fmt.Sprintf("images/%03d.png", randomNumber)
+}
+
 func main() {
-    // Replace with your Telegram bot token
     botToken := "7222244368:AAGEINzLHL6I2rb_dIaLHhb23ONzTKxb_Ng"
+    // botToken := "6999330751:AAEGx91n9_sPFBRxa8K1DXEgVfvuF6YXUXs"
 
     bot, err := tgbotapi.NewBotAPI(botToken)
     if err != nil {
@@ -48,11 +52,16 @@ func main() {
     rand.Seed(time.Now().UnixNano())
 
     for update := range updates {
-        if update.Message == nil { // ignore any non-Message updates
+        if update.Message == nil {
             continue
         }
 
         switch {
+        case strings.HasPrefix(update.Message.Text, "/start"):
+            welcomeMsg := "🌟 Welcome to Just Little Things! 🌟\n\nHere, we celebrate the small joys that make life beautiful. Use the following commands:\n\n/littlething - Get a random image\n/littlething [number] - Get a specific image (1-1000)\n/discoverjoy - Another way to get a random image\n/help - See available commands\n\nEnjoy exploring the simple pleasures!"
+            msg := tgbotapi.NewMessage(update.Message.Chat.ID, welcomeMsg)
+            bot.Send(msg)
+
         case strings.HasPrefix(update.Message.Text, "/littlething"):
             args := strings.Fields(update.Message.Text)
             if len(args) > 1 {
@@ -61,23 +70,20 @@ func main() {
                     imageURL := fmt.Sprintf("images/%03d.png", num)
                     sendMediaGroup(bot, update.Message.Chat.ID, imageURL)
                 } else {
-                    randomNumber := rand.Intn(1000) + 1
-                    imageURL := fmt.Sprintf("images/%03d.png", randomNumber)
-                    sendMediaGroup(bot, update.Message.Chat.ID, imageURL)
+                    sendMediaGroup(bot, update.Message.Chat.ID, getRandomImage())
                 }
             } else {
-                randomNumber := rand.Intn(1000) + 1
-                imageURL := fmt.Sprintf("images/%03d.png", randomNumber)
-                sendMediaGroup(bot, update.Message.Chat.ID, imageURL)
+                sendMediaGroup(bot, update.Message.Chat.ID, getRandomImage())
             }
-        case update.Message.Text == "/start":
-            welcomeMsg := "🌟 Welcome to Just Little Things! 🌟\n\nHere, we celebrate the small joys that make life beautiful. To get started, type /littething [number] to view a specific delight (1-1000), or simply /littething for a random moment of happiness. Enjoy exploring the simple pleasures right under your nose!"
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, welcomeMsg)
-            bot.Send(msg)
+
+        case update.Message.Text == "/discoverjoy":
+            sendMediaGroup(bot, update.Message.Chat.ID, getRandomImage())
+
         case update.Message.Text == "/help":
-            helpMsg := "🛠 Need some help? No worries! 🛠\n\nUse /littething [number] to view a specific image (1-1000) or simply /littething for a random surprise. Let's discover life's little joys together!"
+            helpMsg := "🛠 Available commands: 🛠\n\n/start - Welcome message and instructions\n/littlething - Get a random image\n/littlething [number] - Get a specific image (1-1000)\n/discoverjoy - Another way to get a random image\n/help - See this list of commands"
             msg := tgbotapi.NewMessage(update.Message.Chat.ID, helpMsg)
             bot.Send(msg)
+
         default:
             unknownMsg := "🤔 Hmm, I didn't quite catch that. Type /help to see what I can do for you!"
             msg := tgbotapi.NewMessage(update.Message.Chat.ID, unknownMsg)
